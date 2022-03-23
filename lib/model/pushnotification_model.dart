c7d4f92bd6693f214170fb9390caa0ebe20ef97e
AyushSantri <ayushsantri222@gmail.com>
2022-03-23 20:49:43 +0530

    created a model class

---
+++ lib/model/pushnotification_model.dart
@@ -0,0 +1,12 @@
+class PushNotification {
+  final String title;
+  final String body;
+  final String dataTitle;
+  final String dataBody;
+
+  PushNotification(
+      {required this.title,
+      required this.body,
+      required this.dataTitle,
+      required this.dataBody});
+}
