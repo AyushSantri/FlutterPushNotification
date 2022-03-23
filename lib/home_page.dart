e3df4b991c44393260d9ae1c2d95647d3f6d1d60
AyushSantri <ayushsantri222@gmail.com>
2022-03-23 21:11:54 +0530

    called pushnotification model in home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:push_notification_firebase/model/pushnotification_model.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,6 +13,9 @@ class _HomePageState extends State<HomePage> {
   //initializing FirebaseMessaging
   late final FirebaseMessaging _messaging;
   late int _totalNotificationCounter;
+
+  //calling model
+  PushNotification? _notificationInfo;
 
   @override
   Widget build(BuildContext context) {
