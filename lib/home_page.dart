cec16863f1ca095249de40bd65b962e04b7f2b67
AyushSantri <ayushsantri222@gmail.com>
2022-03-23 21:09:55 +0530

    initialized firebase messaging

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,6 +9,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //initializing FirebaseMessaging
+  late final FirebaseMessaging _messaging;
+  late int _totalNotificationCounter;
+
   @override
   Widget build(BuildContext context) {
     return Container();
